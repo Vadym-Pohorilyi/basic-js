1480d15bdcdfdc4180ac6f3621e62739c12349e0
Vadym Pohorilyi <pogorely.vadim@gmail.com>
2022-10-17 13:36:44 +0300

    feat: add solution

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -1,19 +1,14 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Given some integer, find the maximal number you can obtain
- * by deleting exactly one digit of the given number.
- *
- * @param {Number} n
- * @return {Number}
- *
- * @example
- * For n = 152, the output should be 52
- *
- */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  let numbers = []
+  let nArr = String(n).split('')
+  for (let i = 0; i < nArr.length; i++) {
+    let arr = [...nArr]
+    arr.splice(i, 1)
+    numbers.push(+arr.join(''))
+  }
+  return Math.max(...numbers)
 }
 
 module.exports = {
--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -1,21 +1,15 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-/**
- * Implement class DepthCalculator with method calculateDepth
- * that calculates deoth of nested array
- * 
- * @example
- * 
- * const depthCalc = new DepthCalculator();
- * depthCalc.calculateDepth([1, 2, 3, 4, 5]) => 1
- * depthCalc.calculateDepth([1, 2, 3, [4, 5]]) => 2
- * depthCalc.calculateDepth([[[]]]) => 3
- *
- */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+calculateDepth(arr) {
+  let counter = 0
+  if (Array.isArray(arr)) {
+    for (let i = 0; i < arr.length; i++) {
+      counter = Math.max(counter, this.calculateDepth(arr[i]))
+    }
+    counter++
+  }
+  return counter
   }
 }
 
